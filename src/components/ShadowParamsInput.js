c68af99e0d9f7c83077628fdd80c6332a1682cf6
karim kamel <karim.kamel@live.com>
2021-05-07 04:42:41 +0200

    set min val to blur and spread sliders to zero

--- src/components/ShadowParamsInput.js
+++ src/components/ShadowParamsInput.js
@@ -1,72 +1,77 @@
-import React from 'react';
-import InputSlider from './InputSlider';
-import { SketchPicker } from 'react-color';
-import { Grid } from '@material-ui/core';
+import React from "react";
+import InputSlider from "./InputSlider";
+import { SketchPicker } from "react-color";
+import { Grid } from "@material-ui/core";
 
 export default function ShadowParamsInput({
-	index,
-	handleValueChange,
-	handleColorChange,
-	color,
-	shadow,
+  index,
+  handleValueChange,
+  handleColorChange,
+  color,
+  shadow,
 }) {
-	const shadowSliderParams = {
-		minVal: -50,
-		maxVal: 50,
-		// defaultVal: 10,
-	};
-	const { blur, spread, hOffset, vOffset } = shadow;
+  const offsetSliderParams = {
+    minVal: -50,
+    maxVal: 50,
+    // defaultVal: 10,
+  };
+  const effectSliderParams = {
+    minVal: 0,
+    maxVal: 50,
+    // defaultVal: 10,
+  };
+  const { blur, spread, hOffset, vOffset } = shadow;
 
-	return (
-		<Grid container spacing={3}>
-			<Grid item xs={6}>
-				<InputSlider
-					params={shadowSliderParams}
-					name='hOffset'
-					label='X-offset'
-					value={hOffset}
-					handleOnChange={(event, value, name) =>
-						handleValueChange(event, value, name, index)
-					}
-				/>
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={6}>
+        <InputSlider
+          params={offsetSliderParams}
+          name="hOffset"
+          label="X-offset"
+          value={hOffset}
+          handleOnChange={(event, value, name) =>
+            handleValueChange(event, value, name, index)
+          }
+        />
 
-				<InputSlider
-					params={shadowSliderParams}
-					name='vOffset'
-					label='Y-offset'
-					value={vOffset}
-					handleOnChange={(event, value, name) =>
-						handleValueChange(event, value, name, index)
-					}
-				/>
+        <InputSlider
+          params={offsetSliderParams}
+          name="vOffset"
+          label="Y-offset"
+          value={vOffset}
+          handleOnChange={(event, value, name) =>
+            handleValueChange(event, value, name, index)
+          }
+        />
 
-				<InputSlider
-					params={shadowSliderParams}
-					name='blur'
-					label='blur'
-					value={blur}
-					handleOnChange={(event, value, name) =>
-						handleValueChange(event, value, name, index)
-					}
-				/>
+        <InputSlider
+          params={effectSliderParams}
+          name="blur"
+          label="blur"
+          value={blur}
+          handleOnChange={(event, value, name) =>
+            handleValueChange(event, value, name, index)
+          }
+        />
 
-				<InputSlider
-					params={shadowSliderParams}
-					name='spread'
-					label='spread'
-					value={spread}
-					handleOnChange={(event, value, name) =>
-						handleValueChange(event, value, name, index)
-					}
-				/>
-			</Grid>
+        <InputSlider
+          params={effectSliderParams}
+          name="spread"
+          label="spread"
+          value={spread}
+          handleOnChange={(event, value, name) =>
+            handleValueChange(event, value, name, index)
+          }
+        />
+      </Grid>
 
-			<Grid item xs={6}>
-				<SketchPicker
-					color={color}
-					onChange={(color) => handleColorChange(color, index)}
-				/>
-			</Grid>
-		</Grid>
-	);
+      <Grid item xs={6}>
+        <SketchPicker
+          color={color}
+          onChange={(color) => handleColorChange(color, index)}
+        />
+      </Grid>
+    </Grid>
+  );
 }
